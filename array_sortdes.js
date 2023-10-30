5136fefd4114f71ed2cd58cf3261f17dd41b2fc4
Niharika31Saxena <118336100+Niharika31Saxena@users.noreply.github.com>
2023-10-30 19:38:51 +0530

    ArraySorting

---
+++ array_sortdes.js
@@ -0,0 +1,19 @@
+function sortDescending(arr) {
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+const array = [5, 2, 9, 1, 5, 6];
+sortDescending(array);
+
+console.log(array); 
